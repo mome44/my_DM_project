2a5ee31c5be8fd33cb269e65512bfc05b9cb748c
SimoneLagana <lagana.1946083@studenti.uniroma1.it>
2024-09-29 19:23:40 +0200

    ETL part 2

--- ETL.py
+++ ETL.py
@@ -1,102 +1,133 @@
 import pandas as pd
 from openpyxl import load_workbook, Workbook
-from geopy.geocoders import Nominatim
-from time import sleep
-geolocator = Nominatim(user_agent="geoapi")
-
-def get_lat_long(location_name):
-    try:
-        location = geolocator.geocode(location_name, timeout=10)
-        if location:
-            return location.latitude, location.longitude
-        else:
-            return None, None
-    except Exception as e:
-        print(f"Error fetching location for {location_name}: {e}")
-        return None, None
-
-#temperature_table = pd.read_csv('Environment_Temperature_change_E_All_Data_NOFLAG.csv', encoding='latin1')
-#emission_table_1= pd.read_csv('GCB2022v27_MtCO2_flat.csv', encoding='latin1')
-#emission_table_2= pd.read_csv('GCB2022v27_percapita_flat.csv', encoding='latin1')
-#emission_table_3 = pd.read_csv('GCB2022v27_sources_flat.csv', encoding='latin1')
-#
-#events=load_workbook('public_emdat_custom_request_2024-06-26_65811dbf-f6a0-43b0-a0af-a151ab0b3ee7.xlsx').active
-#print (temperature_table.columns)
-#print (emission_table_1.columns)
-#print (emission_table_2.columns)
-#print (emission_table_3.columns)
-
-#temp_cols=temperature_table.columns.to_list()
-#emiss_cols=emission_table_1.columns.to_list()
-#emiss_cols_2=emission_table_2.columns.to_list()
-
-#for i in range(len(events)):
- #   print(events[i][0].value)
-
-MISSING_STRING ='Unknown'
-MISSING_STRING_2 ='Not specified'
-MISSING_NUMBER = 'Not_related'
-
-#ECONOMY TABLE -- CLEANING
-economy= pd.read_csv('datasets/economic-damage-from-natural-disasters.csv', encoding='latin1')
-print (economy.columns)
-# Drop the 'Code' column since it's not useful in this dataset
-economy_cleaned = economy.drop(columns=['Code'])
-
-# Now the dataset is cleaned of the 'Code' column, you can proceed with further transformations if needed
-# For example, checking for missing values or converting the 'Total economic damages' column to numeric
-economy_cleaned['Total economic damages'] = pd.to_numeric(economy_cleaned['Total economic damages'], errors='coerce')
-
-# Check for remaining missing values
-missing_values = economy_cleaned.isnull().sum()
-
-# Save the cleaned dataset (optional)
-economy_cleaned.to_csv('cleaned_datasets/cleaned_economic_damage.csv', index=False)
-
-print(economy_cleaned.head())  # Display the cleaned dataset
-
-#EMDAT TABLE --CLEANING
-events= pd.read_excel('datasets/public_emdat_custom_request_2024-06-26_65811dbf-f6a0-43b0-a0af-a151ab0b3ee7.xlsx')
-print(events.columns)
-
-events_cleaned= events.drop(columns=['DisNo.', 'Historic', 'External IDs', 'Origin', 'Associated Types', 'River Basin','CPI', 'Admin Units', 'Entry Date', 'Last Update'])
-print(events_cleaned.head())
+import json
 
 
-events_cleaned['Event Name'].fillna(MISSING_STRING, inplace=True)
-events_cleaned['Location'].fillna(MISSING_STRING_2, inplace=True)
-events_cleaned['Location']=events_cleaned['Location'].str.split(',')
-events_cleaned = events_cleaned.explode('Location')
-events_cleaned['Location'].str.strip(' ')
-missing_condition = events_cleaned['Magnitude'].isnull() | events_cleaned['Magnitude Scale'].isnull()
-events_cleaned.loc[missing_condition, ['Magnitude', 'Magnitude Scale']] = MISSING_STRING
+#CONSTANTS
+MISSING_STRING='unknown'
+MISSING_NUMBER=0
+MISSING_STRING_2='Not specified'
 
-country_coords={}
-for country in events_cleaned['Country'].unique():
-    lat,lon=get_lat_long(country)
-    country_coords[country] = (lat,lon)
+def load_location_coords(path):
+    with open(path, 'r') as file:
+        country_coords = json.load(file)
+    return country_coords
 
-for idx, row in events_cleaned.iterrows():
-    if pd.isnull(row['Latitude']) or pd.isnull(row['Longitude']):
-        country_name = row['Country']
-        lat, lon = country_coords.get(country_name, (None, None))
-        events_cleaned.at[idx, 'Latitude'] = lat
-        events_cleaned.at[idx, 'Longitude'] = lon
+def clean_economy(economy):
+    print (economy.columns)
+    economy_cleaned = economy.drop(columns=['Code'])
+    economy_cleaned['Total economic damages'] = pd.to_numeric(economy_cleaned['Total economic damages'], errors='coerce')
+    missing_values = economy_cleaned.isnull().sum()
+    return economy_cleaned
 
-events_cleaned['AID Contribution (\'000 US$)'].fillna(0, inplace=True)
-events_cleaned['Total Damage, Adjusted (\'000 US$)'].fillna(0, inplace=True)
-events_cleaned['Total Damage (\'000 US$)'].fillna(0, inplace=True)
-events_cleaned['Insured Damage (\'000 US$)'].fillna(0, inplace=True)
-events_cleaned['Insured Damage, Adjusted (\'000 US$)'].fillna(0, inplace=True)
-events_cleaned['Reconstruction Costs (\'000 US$)'].fillna(0, inplace=True)
-events_cleaned['Reconstruction Costs, Adjusted (\'000 US$)'].fillna(0, inplace=True)
+def clean_event(event):
+    for c in event.columns:
+        print(pd.api.types.is_numeric_dtype(event[c]), c)
 
-events_cleaned.to_csv('cleaned_datasets/cleaned_emdat_dataset.csv', index=False)
+    #DELETING USELESS COLUMNS
+    events_cleaned = event.drop(columns=['Historic', 'External IDs', 'Origin', 'Associated Types', 'River Basin','CPI', 'Admin Units', 'Entry Date', 'Last Update'])
+    
+    #CONVERT COLUMNS TO NUMERIC
+    numeric_columns=['Latitude', 'Longitude', 'Magnitude', 'Start Year', 'Start Month','Start Day', 'End Year', 'End Month', 'End Day', 'Total Deaths'
+                     ,'No. Injured', 'No. Affected', 'No. Homeless', 'Total Affected', 'AID Contribution (\'000 US$)', 'Total Damage, Adjusted (\'000 US$)'
+                     ,'Total Damage (\'000 US$)', 'Insured Damage (\'000 US$)','Insured Damage, Adjusted (\'000 US$)', 'Reconstruction Costs (\'000 US$)'
+                     , 'Reconstruction Costs, Adjusted (\'000 US$)']
+    for col in numeric_columns:
+        events_cleaned[col] = pd.to_numeric(events_cleaned[col], errors='coerce')
 
-missing_values_count = events_cleaned.isnull().sum()
+    events_cleaned['Location'].fillna(MISSING_STRING_2, inplace=True)
+    events_cleaned['Location']=events_cleaned['Location'].str.split(',')
+    events_cleaned = events_cleaned.explode('Location')
+    events_cleaned['Location']=events_cleaned['Location'].str.strip()
 
-# Print the columns with their corresponding missing values
-print("Columns with Missing Values:")
-for column, missing_count in missing_values_count.items():
-    if missing_count>0:
-        print(f"{column}: {missing_count} missing values")
+    location_coords=load_location_coords('utils/country_coords.json')
+
+    for idx, row in events_cleaned.iterrows():
+        if pd.isnull(row['Latitude']) or pd.isnull(row['Longitude']):
+            lat, lon = location_coords.get(str(idx))
+            print(idx,lat,lon)
+            events_cleaned.at[idx, 'Latitude'] = lat
+            events_cleaned.at[idx, 'Longitude'] = lon
+
+    events_cleaned=normalize_table(events_cleaned)
+    
+    return events_cleaned
+
+def clean_temperature(temperature):
+
+    df_melted = pd.melt(temperature, 
+                    id_vars=['Area Code', 'Area', 'Months Code', 'Months', 'Element', 'Unit','Element Code'], 
+                    var_name='Year', 
+                    value_name='Value')
+
+    # Convert the 'Year' column from 'Y1961' to just '1961'
+    df_melted['Year'] = df_melted['Year'].str.replace('Y', '').astype(int)
+
+    # Pivot the 'Element' column to create 'Temperature change' and 'Standard deviation' as separate columns
+    df_pivoted = df_melted.pivot_table(index=['Area Code', 'Area', 'Months Code', 'Months', 'Year'], 
+                                       columns='Element', 
+                                       values='Value').reset_index()
+
+    # Rename the columns for clarity
+    df_pivoted.columns.name = None
+    df_pivoted.rename(columns={'Temperature change': 'Temperature Change', 
+                           'Standard deviation': 'Standard Deviation'}, inplace=True)
+    cleaned_temperature=df_pivoted
+    cleaned_temperature=normalize_table(cleaned_temperature)
+    cleaned_temperature=cleaned_temperature.drop(columns=['Area Code', 'Months Code'])
+    for column in cleaned_temperature.columns:
+        missing_values = cleaned_temperature[column].isnull().sum()
+        print(f"Column: {column}, Missing Values: {missing_values}")
+    return cleaned_temperature
+
+
+
+
+
+
+
+
+def normalize_table(table):
+    for column in table.columns:
+        if table[column].isnull().any():
+            if table[column].dtype == 'object':
+                table[column].fillna(MISSING_STRING, inplace=True)
+                table[column] = table[column].str.lower().str.strip()
+
+            elif pd.api.types.is_numeric_dtype(table[column]):
+                table[column].fillna(MISSING_NUMBER, inplace=True)
+
+            elif table[column].dtype == 'bool':
+                table[column].fillna(False, inplace=True)
+    return table
+
+def clean_emission(emission):
+    cleaned_emission=normalize_table(emission)
+    return cleaned_emission
+
+
+if __name__=='__main__':
+    economy_table= pd.read_csv('datasets/economic-damage-from-natural-disasters.csv', encoding='latin1')
+    event_table= pd.read_excel('datasets/public_emdat_custom_request_2024-06-26_65811dbf-f6a0-43b0-a0af-a151ab0b3ee7.xlsx')
+    temperature_table = pd.read_csv('datasets/Environment_Temperature_change_E_All_Data_NOFLAG.csv', encoding='latin1')
+    emission_table_1= pd.read_csv('datasets/GCB2022v27_MtCO2_flat.csv', encoding='latin1')
+    emission_table_2= pd.read_csv('datasets/GCB2022v27_percapita_flat.csv', encoding='latin1')
+    emission_table_3 = pd.read_csv('datasets/GCB2022v27_sources_flat.csv', encoding='latin1')
+
+    ##CLEANING economy table
+    print('starting to clean economy table')
+    economy_cleaned= clean_economy(economy_table)
+    economy_cleaned.to_csv('cleaned_datasets/cleaned_economic_damage.csv', sep=';', decimal=',', index=False)
+    print(economy_cleaned.head())
+
+    #CLEANING event table
+    print('starting to clean event table')
+    event_cleaned=clean_event(event_table)
+    event_cleaned.to_csv('cleaned_datasets/cleaned_event.csv', sep=';', decimal=',', index=False)
+    print(event_cleaned.head())
+    
+    #CLEANING temperature table
+    print ('starting to clean temperature table')
+    temperature_table_cleaned=clean_temperature(temperature_table)
+    temperature_table_cleaned.to_csv('cleaned_datasets/cleaned_temperature.csv', sep=';', decimal=',', index=False)
+    
--- coordinates.py
+++ coordinates.py
@@ -2,64 +2,83 @@ import pandas as pd
 from geopy.geocoders import Nominatim
 import json
 from time import sleep
-
+from datetime import datetime
 # Initialize the geolocator
 geolocator = Nominatim(user_agent="geoapi")
 
-events= pd.read_excel('datasets/public_emdat_custom_request_2024-06-26_65811dbf-f6a0-43b0-a0af-a151ab0b3ee7.xlsx')
-
 # Function to get latitude and longitude for a given country
-def get_lat_long(location_name):
+def get_coordinates(location):
     try:
-        location = geolocator.geocode(location_name, timeout=10)
-        if location:
-            return location.latitude, location.longitude
+        coordinates = geolocator.geocode(location, timeout=10)
+        if coordinates:
+            return coordinates.latitude, coordinates.longitude
         else:
             return None, None
     except Exception as e:
-        print(f"Error fetching location for {location_name}: {e}")
+        print(f"Error fetching location for {location}: {e}")
         return None, None
 
-# Step 1: Create and populate the country_coords dictionary
-def create_country_coords(df):
-    country_coords = {}
-    for country in df['Country'].unique():
-        lat, lon = get_lat_long(country)
-        country_coords[country] = (lat, lon)
-        # Sleep to avoid overwhelming the geolocation service
-        sleep(1)
-    return country_coords
+def generate_coords(table):
+    print(f"Unique values in 'Location': {table['Location'].nunique()}")
+    print(f"Unique values in 'Country': {table['Country'].nunique()}")
+    print(f"Unique values in 'ISO': {table['ISO'].nunique()}")
+    location_coords = {}
+    index_row=0
+    for i,row in table.iterrows():
+        location = row['Location']
+        country = row['Country']
+        iso_code = row['ISO']
+        lat = None
+        lon = None
+        #print(location, country, iso_code)
+        if pd.notnull(location):
+            lat, lon = get_coordinates(location)
+            #print(f"Attempt for Location '{location}': {lat}, {lon}")
+        if (lat is None) and (lon is None) and pd.notnull(country):
+            #print(f"Location '{location}' not found. Retrying with Country: {country}")
+            lat, lon = get_coordinates(country)
+            #print(f"Attempt for Country '{country}': {lat}, {lon}")
+        if (lat is None) and (lon is None) and pd.notnull(iso_code):
+            #print(f"Country '{country}' not found. Retrying with ISO: {iso_code}")
+            lat, lon = get_coordinates(iso_code)
+            #print(f"Attempt for ISO '{iso_code}': {lat}, {lon}")
+        
+        if (lat is not None) and (lon is not None):
+            lat = str(lat).replace('.', ',')
+            lon = str(lon).replace('.', ',')
+        if index_row % 450 == 0:
+            current_time = datetime.now().strftime("%H:%M:%S")
+            print(f"{index_row/45000 *100}% -- {current_time}")
+        location_coords[index_row] = (lat, lon)
+        index_row+=1
+        #sleep(1)
+    
+    return location_coords
 
-# Step 2: Save the dictionary to a file (JSON format)
 def save_country_coords_to_file(country_coords, file_path):
     with open(file_path, 'w') as file:
         json.dump(country_coords, file)
 
-# Step 3: Load the dictionary from a file
 def load_country_coords_from_file(file_path):
     with open(file_path, 'r') as file:
         country_coords = json.load(file)
     return country_coords
 
-# Step 4: Use the loaded dictionary to fill in the Latitude and Longitude columns
-def fill_lat_long_from_dict(df, country_coords):
-    for idx, row in df.iterrows():
+
+def fill_lat_long_from_dict(table, country_coords):
+    for idx, row in table.iterrows():
         if pd.isnull(row['Latitude']) or pd.isnull(row['Longitude']):
             country_name = row['Country']
             lat, lon = country_coords.get(country_name, (None, None))
-            df.at[idx, 'Latitude'] = lat
-            df.at[idx, 'Longitude'] = lon
-    return df
+            table.at[idx, 'Latitude'] = lat
+            table.at[idx, 'Longitude'] = lon
+    return table
 
-# Example DataFrame (replace with your EMDAT dataset)
-data = {'Country': ['United States', 'Japan', 'Jamaica', 'Brazil', 'Japan'],
-        'ISO': ['USA', 'JPN', 'JAM', 'BRA', 'JPN'],
-        'Latitude': [None, None, None, None, None],
-        'Longitude': [None, None, None, None, None]}
-df = pd.DataFrame(data)
-
-# Create the country_coords dictionary (this only needs to be done once)
-country_coords = create_country_coords(df)
-
-# Save the dictionary to a JSON file
-save_country_coords_to_file(country_coords, 'country_coords.json')
+if __name__ == "__main__":
+    events= pd.read_excel('datasets/public_emdat_custom_request_2024-06-26_65811dbf-f6a0-43b0-a0af-a151ab0b3ee7.xlsx')
+    #events['Location'].fillna('UNKNOWN', inplace=True)
+    events['Location']=events['Location'].str.split(',')
+    events = events.explode('Location')
+    events['Location'].str.lstrip(' ')
+    country_coords = generate_coords(events)
+    save_country_coords_to_file(country_coords, 'utils/country_coords.json')
